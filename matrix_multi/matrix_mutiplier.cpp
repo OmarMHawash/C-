13f89566267f503e8ef38477e86912fcbc63544e
Omar M. Hawash <omarmhawash@Omars-MacBook-Air.local>
2022-01-31 13:20:31 +0200

    clearer ver

--- matrix_multi/matrix_mutiplier.cpp
+++ matrix_multi/matrix_mutiplier.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
-#include <string>
-#include <cmath>
 using namespace std;
 
 int main() {
-    float B[2][4]={
-        {1,3,7,9},//
+    int m1[2][4]={
+        {1,3,7,9},
         {6,4,2,1},
     },
-    A[4][3]={
-        //
+    m2[4][3]={
         {1,2,1},
         {0,1,0},
         {1,1,2},
         {0,0,1},
     },
-    C[2][3],sum;
+    multiResult[2][3],sum;
     
     for(int i=0;i<2;i++){
         for(int j=0;j<3;j++){
             sum=0;
             for(int r=0;r<4;r++){
-                sum+=B[i][r]*A[r][j];
+                sum+=m1[i][r]*m2[r][j];
             }
-            C[i][j]=sum;
+            multiResult[i][j]=sum;
         };
     }cout<<endl;
 
     for(int i=0;i<2;i++){
-        cout<<"|";
+        cout<<"| ";
         for(int j=0;j<3;j++){
-            cout<<C[i][j]<<" ";
+            cout<<multiResult[i][j]<<" ";
         }
         cout<<"|"<<endl;
     }cout<<endl;
